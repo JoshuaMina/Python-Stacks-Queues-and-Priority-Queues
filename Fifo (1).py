1704e3862c64188e307711fd6bd4ee6cf6b02437
JoshuaMina <joshua.minacambronero@gmail.com>
2022-12-12 07:13:48 +0800

    LiFo

--- Fifo (1).py
+++ Fifo (1).py
@@ -1,13 +1,15 @@
 from queuesSecond import Queue
 
 fifo = Queue("1st", "2nd", "3rd", "4th")
-print(len(fifo))
+print("Number of Current Queues:",len(fifo))
 
 
 for element in fifo:
     print(element)
 
+# The queue has three elements initially, but its length drops to zero
+# after consuming all elements in a loop. Next up, you’ll implement a
+# stack data type that’ll dequeue elements in reverse order.
 
 
-
-print(len(fifo))
+print("Number of Current Queues:",len(fifo))
---
+++ Lifo.py
@@ -0,0 +1,9 @@
+from queuesThird import Stack
+
+lifo = Stack("1st", "2nd", "3rd")
+print("Number of Current Queues:",len(lifo))
+
+for element in lifo:
+    print(element)
+
+print("Number of Current Queues:",len(lifo))
