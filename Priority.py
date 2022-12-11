1b714f0cfac239d096ed78646e0a912055abb9fd
JoshuaMina <joshua.minacambronero@gmail.com>
2022-12-12 07:34:55 +0800

    Priority Pop

--- Priority.py
+++ Priority.py
@@ -1,8 +1,15 @@
-from heapq import heappush
+from heapq import heappush, heappop
 
 fruits = []
 heappush(fruits, "orange")
 heappush(fruits, "apple")
 heappush(fruits, "banana")
 
-print(fruits)
+print("The list of Queue",fruits)
+
+heappop(fruits)
+
+
+print("The list of Queue",fruits)
+#When you pop an element from a heap, you’ll always get the first one,
+# while the remaining elements might shuffle a little bit
