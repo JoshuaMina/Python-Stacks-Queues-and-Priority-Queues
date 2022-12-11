5c3a8e0555eb9b43c71b5117048f780e964137e9
JoshuaMina <joshua.minacambronero@gmail.com>
2022-12-12 07:20:29 +0800

    Plain Python List Stack

---
+++ Lifo (1).py
@@ -0,0 +1,19 @@
+#In one-off scripts, you could probably get away with using a plain
+# old Python list as a rudimentary stack when you don’t mind the extra
+# overhead of having to copy the values from time to time:
+lifo = []
+
+lifo.append("1st")
+lifo.append("2nd")
+lifo.append("3rd")
+
+print(lifo)
+
+lifo.pop()
+print("The '3rd' element has been Dequeued",lifo)
+
+lifo.pop()
+print("The '2nd' element has been Dequeued",lifo)
+
+lifo.pop()
+print("The '1st' element has been Dequeued",lifo)
