fedcd68b971c3d15a0ff7e0ed13303c3c7a9eb83
JoshuaMina <joshua.minacambronero@gmail.com>
2022-12-12 18:42:25 +0800

    Thread Safe Queues

---
+++ thread_safe_queues.py
@@ -0,0 +1,55 @@
+
+import argparse
+from queueMixin import Stack, PriorityQueue, Queue
+import threading
+from random import randint
+from time import sleep
+
+QUEUE_TYPES = {
+    "fifo": Queue,
+    "lifo": Stack,
+    "heap": PriorityQueue
+}
+
+def main(args):
+    buffer = QUEUE_TYPES[args.queue]()
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-q", "--queue", choices=QUEUE_TYPES, default="fifo")
+    parser.add_argument("-p", "--producers", type=int, default=3)
+    parser.add_argument("-c", "--consumers", type=int, default=2)
+    parser.add_argument("-ps", "--producer-speed", type=int, default=1)
+    parser.add_argument("-cs", "--consumer-speed", type=int, default=1)
+    return parser.parse_args()
+
+if __name__ == "__main__":
+    try:
+        main(parse_args())
+    except KeyboardInterrupt:
+        pass
+PRODUCTS = (
+    ":balloon:",
+    ":cookie:",
+    ":crystal_ball:",
+    ":diving_mask:",
+    ":flashlight:",
+    ":gem:",
+    ":gift:",
+    ":kite:",
+    ":party_popper:",
+    ":postal_horn:",
+    ":ribbon:",
+    ":rocket:",
+    ":teddy_bear:",
+    ":thread:",
+    ":yo-yo:",
+)
+class Worker(threading.Thread):
+    def __init__(self, speed, buffer):
+        super().__init__(daemon=True)
+        self.speed = speed
+        self.buffer = buffer
+        self.product = None
+        self.working = False
+        self.progress = 0
