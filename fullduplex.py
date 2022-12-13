1b2e1beb0c44430a0effdccc7d416385e27f7213
JoshuaMina <joshua.minacambronero@gmail.com>
2022-12-13 16:48:33 +0800

    Full Duplex

--- fullduplex.py
+++ fullduplex.py
@@ -8,6 +8,8 @@ from string import ascii_lowercase
 import multiprocessing
 from dataclasses import dataclass
 import argparse
+import queue
+import time
 
 
 def reverse_md5(hash_value, alphabet=ascii_lowercase, max_length=6):
@@ -45,6 +47,8 @@ def reverse_md5(hash_value, alphabet=ascii_lowercase, max_length=6):
 
 
 def main(args):
+    t1 = time.perf_counter()
+
     queue_in = multiprocessing.Queue()
     queue_out = multiprocessing.Queue()
 
@@ -60,6 +64,18 @@ def main(args):
         combinations = Combinations(ascii_lowercase, text_length)
         for indices in chunk_indices(len(combinations), len(workers)):
             queue_in.put(Job(combinations, *indices))
+
+    while any(worker.is_alive() for worker in workers):
+        try:
+            solution = queue_out.get(timeout=0.1)
+            if solution:
+                t2 = time.perf_counter()
+                print(f"{solution} (found in {t2 - t1:.1f}s)")
+                break
+        except queue.Empty:
+            pass
+    else:
+        print("Unable to find a solution")
 
 def parse_args():
     parser = argparse.ArgumentParser()
